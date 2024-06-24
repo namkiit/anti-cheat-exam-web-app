2de678cad42f6bfd7f70fad91fc26e66135e6c63
namkiit <namkiet3010@gmail.com>
2024-06-24 15:06:51 +0700

    add description to dashboard page

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -19,6 +19,8 @@ const Dashboard: React.FC<DashboardProps> = ({ loadingBarRef }) => {
       <h1 className={classes.title}>Xin chào {session.data?.user?.fname} !</h1>
 
       <Grid container direction="column" spacing={4}>
+        <div style={{ textAlign: 'center', marginTop: '40px' }}>{assignedExams.length === 0 ? "Bạn không có bài kiểm tra nào được giao !" : "Bạn còn những bài kiểm tra sau cần hoàn thành: "}</div>
+
         {assignedExams.map((exam) => (
           <Grid key={exam._id} item>
             <ExamCard exam={exam} loadingBarRef={loadingBarRef} />
