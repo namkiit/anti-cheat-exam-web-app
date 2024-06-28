ca32a47d5c04a5f3c299f18b8416cedd3f10372a
namkiit <namkiet3010@gmail.com>
2024-06-28 09:38:04 +0700

    config next to not remove debugger statement

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
+const TerserPlugin = require('terser-webpack-plugin');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
     BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_debugger: false, // Do not remove debugger statements
+            },
+          },
+        }),
+      ];
+    }
+    return config;
   },
 };
 
